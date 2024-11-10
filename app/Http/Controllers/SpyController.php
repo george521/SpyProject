36a51cad012e92c45aed92ede2e80feaec8ff520
george521 <george.aggelopoulos@hotmail.com>
2024-11-10 19:45:24 +0200

    list with filters, sort & pagination

--- app/Http/Controllers/SpyController.php
+++ app/Http/Controllers/SpyController.php
@@ -34,4 +34,53 @@ class SpyController
         return response()->json($spies, 200);
     }
 
+    public function list(Request $request){
+        $filters = $request->input('filters') ?? [];
+        $sorting = $request->input('sort')?? [];
+        $fetch = $request->input('fetch')?? null;
+
+        try {
+            $qry='';
+            foreach ($filters as $filter){
+                if($filter == 'name'){
+                    $qry .= ' name = "'. $request->input('name') .'" AND ';
+                }elseif($filter == 'surname'){
+                    $qry .= ' surname = "'. $request->input('surname').'" AND ';
+                }
+                elseif($filter == 'exact_age'){
+                    $qry .= ' TIMESTAMPDIFF(YEAR, date_of_birth, CURDATE()) = '.$request->input('age').' AND ';
+                }elseif($filter == 'range_age'){
+                    $qry .= ' TIMESTAMPDIFF(YEAR, date_of_birth, CURDATE()) >= '.$request->input('from'). ' AND TIMESTAMPDIFF(YEAR, date_of_birth, CURDATE()) <= '.$request->input('to').' AND ';
+                }
+                else{
+                    return response()->json('Not a supported filter', 500);
+                }
+            }
+            $qry = rtrim($qry, ' AND ');
+            if($qry == '') $qry = '1=1';
+            $spies = SpyModel::whereRaw($qry);
+            foreach ($sorting as $sort){
+                if($sort == 'fullname'){
+                    $spies = $spies->orderBy('name','desc')->orderBy('surname', 'desc');
+                }elseif($sort == 'date_of_birth'){
+                    $spies = $spies->orderBy('date_of_birth', 'desc');
+                }elseif($sort == 'date_of_death'){
+                    $spies = $spies->orderBy('date_of_death', 'desc');
+                }else{
+                    return response()->json('Not a supported sort', 500);
+                }
+            }
+
+
+            if($fetch){
+                $spies = $spies->paginate($fetch)->getCollection();
+            }else{
+                $spies = $spies->get();
+            }
+        }catch (\Exception $ex){
+            return response()->json($ex->getMessage(), 500);
+
+        }
+        return response()->json($spies, 200);
+    }
 }
